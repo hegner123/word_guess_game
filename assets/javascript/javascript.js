f9259258dd2861412d71c9be0a5632bf6db1547c
Michael Hegner <hegner123@gmail.com>
2019-08-16 12:47:25 -0400

    added comments to javascript for clarity, added console logs for testing

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -15,40 +15,42 @@ var wins = 0;
 var losses = 0;
 var guessAtmpt = 12;
 
+
+// array storing key presses
 var userGuessLetters = []
 
+// variable deciding the "word"
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+// display the "word" in the console
+console.log(computerGuess);
 
 document.onkeyup = function(event) {
   
 // variable of user key press
   var userGuess = event.key;
-// array to be used to store guessed letters
-  
 
+  // if a key pressed is already in the array do nothing
   if (userGuessLetters.includes(userGuess)) {
 ;
   }
+  // else, store the key pressed in the array
   else {
     userGuessLetters.push(userGuess);
   }
 
-
+// if the "word" contains the key pressed, display success in the console
 if (computerGuess.includes(userGuess)) {
   console.log("success");
 }
+// if the "word" does not contain the key pressed, display "else success" in the console
 else {
   console.log("else success");
 }
 
-console.log(computerGuess);
-
-  // userGuessLetters.push(userGuess);
 
 
-
-// variable to choose which word is being used in the game
-
+// display the contents of userGuessLetters in the console
+console.log(userGuessLetters);
 
 
 
