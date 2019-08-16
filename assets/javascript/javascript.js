99a1eb6de0bb6c5e7e531413c6e6bdd238c37465
Michael Hegner <hegner123@gmail.com>
2019-08-16 07:16:35 -0400

    added array of shapes, added win, loss, and guess attempts variables

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,12 +1,17 @@
+// defines p elements as variables
 var game  = document.getElementById("game");
 var winsText  = document.getElementById("winsText");
 var loseText  = document.getElementById("loseText");
 var guessText = document.getElementById("guessText");
 var userGuessText = document.getElementById("userGuessText");
 
+// array
+var computerChoices = ["square", "circle", "triangle"];
 
-
-
+// variables to store wins, losses, and number of attempts remaining
+var wins = 0;
+var losses = 0;
+var guessAtmpt = 12;
 
 
 
