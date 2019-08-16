cad54a0d7fa58492a45a0db362e9dcf8003858fa
Michael Hegner <hegner123@gmail.com>
2019-08-16 07:12:33 -0400

    defined p elements as variables in javascript.js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,27 @@
+var game  = document.getElementById("game");
+var winsText  = document.getElementById("winsText");
+var loseText  = document.getElementById("loseText");
+var guessText = document.getElementById("guessText");
+var userGuessText = document.getElementById("userGuessText");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.onkeyup = function(event) {}
